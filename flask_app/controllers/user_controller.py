e2e5a27dfc67c465f7cbb8a16f913dd54f22610f
Hamza Jabli <hamzajbali007@gmail.com>
2024-11-14 14:46:27 +0100

    commit

---
+++ flask_app/controllers/user_controller.py
@@ -0,0 +1,46 @@
+from flask import session,request,render_template,redirect,url_for
+from flask_app import app
+from flask_app.models.user_model import User
+from flask import flash
+from flask_bcrypt import Bcrypt
+
+bcrypt=Bcrypt(app)
+
+
+@app.route('/signUp')
+def index():
+    return render_template("index.html")
+
+@app.route("/new/user",methods=["POST"])
+def add_user():
+    
+    if not User.validate(request.form):
+        return redirect('/signUp')
+    else:
+        pw=bcrypt.generate_password_hash(request.form["password"])
+        data={**request.form,
+              "password": pw}
+        user_id=User.add_one(data)
+        
+        session["user_id"]=user_id
+        return redirect('/home')
+
+@app.route('/login')
+def login():
+    
+    return render_template("login.html")
+
+
+
+@app.route('/user/login', methods=["POST"])
+def submit_login():
+    user=User.get_by_email(request.form)
+    if user==None:
+        return redirect("/")
+    elif not bcrypt.check_password_hash(request.form["password"],user.password):
+            flash("Invalid Email/Password","login_password_validation")
+            return redirect("/")
+    else:
+        session["user_id"]=user.id
+        session["name"]=user.user_name
+        return redirect("/login")
---
+++ flask_app/models/user_model.py
@@ -0,0 +1,59 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app import DATABASE,app
+from flask import flash
+import re
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
+
+class User:
+    def __init__(self,data):
+        self.id=data["id"]
+        self.first_name=data["user_name"]
+        self.email=data["email"]
+        self.password=data["password"]
+        self.type_user=data["type_user"]
+        self.created_at=data["created_at"]
+        self.updated_at=data["updated_at"]
+
+    @classmethod
+    def add_one(cls,data):
+        query="insert into users (user_name, email, password, type_user) values (%(user_name)s, %(email)s, %(password)s, 0);"
+        result=connectToMySQL(DATABASE).query_db(query,data)
+        return result
+
+    
+
+        
+    @staticmethod
+    def validate(data):
+        is_valid=True
+        if len(data["user_name"])<2:
+            is_valid=False
+            flash("user Name needs to have at least 3 characters","user_name")
+        if not EMAIL_REGEX.match(data["email"]): 
+            flash("Invalid email address!","email")
+            is_valid = False
+        elif User.get_by_email({"email":data["email"]}):
+            flash("email already in database","email")
+            is_valid = False
+        if len(data["password"])<8:
+            is_valid=False
+            flash("Password not strong enough","password")
+        return is_valid
+        
+                
+    @classmethod
+    def get_one(cls,data):
+        query="select * from users where id=%(id)s;"
+        result=connectToMySQL(DATABASE).query_db(query,data)
+        return cls(result[0])
+    
+    @classmethod
+    def get_by_email(cls,data):
+        query="select * from users where email=%(email)s;"
+        result=connectToMySQL(DATABASE).query_db(query,data)
+        if len(result)==0:
+            flash("user email not in the database","email_validation_login")
+            return None
+        else:
+            return cls(result[0])
